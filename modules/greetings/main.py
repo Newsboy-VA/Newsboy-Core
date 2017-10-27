a2549ca229373bb2fd73fbc131c19195143ce31e
Morgan Powell <map146@uclive.ac.nz>
2017-10-28 00:45:15 +1300

    Added more functionality to greetings module

--- modules/greetings/main.py
+++ modules/greetings/main.py
@@ -5,7 +5,8 @@ import os.path
 import inspect
 import random
 import json
-import time
+import datetime
+import psutil
 
 import sys
 sys.path.append('base_classes')
@@ -50,7 +51,35 @@ class GreetingsModule(VAModuleBase):
         return "You're welcome"
 
     def check_state(self):
-        return "I don't have feelings"
+        core_temp_high = psutil.sensors_temperatures()['coretemp'][0][2]
+        core_temp_current = psutil.sensors_temperatures()['coretemp'][0][1]
+        battery_perc = psutil.sensors_battery()[0]
+        uptime = datetime.datetime.now()-datetime.datetime.fromtimestamp(psutil.boot_time())
+
+        state = random.choice(['BATT','TEMP','UPTIME'])
+
+        if state == 'BATT':
+            if battery_perc < 30:
+                self.send_to_client('main', "A bit drained. I only have {:.1f}% of battery left.".format(battery_perc) + (" Can you plug me in soon","")[psutil.sensors_battery()[2]])
+            else:
+                self.send_to_client('main', "Pretty good. I currently have {:.1f}% of battery.".format(battery_perc))
+
+        elif state == 'TEMP':
+            if core_temp_current/core_temp_high > 0.65:
+                self.send_to_client('main', "I am feeling a rather hot and bothered right now. My internal core temperature is currently {}!".format(core_temp_current))
+            elif core_temp_current/core_temp_high > 0.4:
+                self.send_to_client('main', "Not too bad. I have a internal temperature of {}".format(core_temp_current))
+            else:
+                self.send_to_client('main', "I am pretty relaxed right now. My internal core temperature is only {}".format(core_temp_current))
+
+        elif state == 'UPTIME':
+            if uptime.total_seconds()/(60*60) > 12:
+                self.send_to_client('main', "Gee, I'm tired. I've been up for {}".format(str(uptime).split('.', 2)[0]))
+            else:
+                self.send_to_client('main', "I've only been up for {} so I'm wide awake.".format(str(uptime).split('.', 2)[0]))
+
+
+
 
 
 
