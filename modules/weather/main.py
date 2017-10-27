2cf4e4c72bc0f5c95332e4ae75cd16f9cd00f816
Morgan Powell <map146@uclive.ac.nz>
2017-10-27 21:21:57 +1300

    Fix bugs in weather module

--- modules/weather/main.py
+++ modules/weather/main.py
@@ -6,32 +6,32 @@ import inspect
 import random
 import json
 import time
+import feedparser
 
 import sys
 sys.path.append('base_classes')
 from module_base import VAModuleBase
 
 
-class GreetingsModule(VAModuleBase):
+class WeatherModule(VAModuleBase):
     def __init__(self):
         module_path = os.path.dirname(inspect.getfile(inspect.currentframe()))
 
         super().__init__(module_path)
 
-        entities_file = open(os.path.join(module_path, "entities.json"))
-        self.entities_json = json.load(entities_file)['entities']
-        entities_file.close()
 
     def background_tasks(self):
         ''' Perform all the background tasks that need to be done '''
         pass
 
     def get_weather(self):
+        # look into using metservice data: https://github.com/arfar/metservice-api-py
+        
         weather_data = feedparser.parse("http://www.rssweather.com/wx/nz/christchurch/rss.php")
         weather_summary = weather_data['entries'][0]['summary_detail']['value']
         return weather_summary
 
 
 if __name__ == "__main__":
-    with GreetingsModule() as module:
+    with WeatherModule() as module:
         pass
