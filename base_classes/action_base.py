3b489b82bd4d97573636e6f39fc9adccbcd35879
Peter Nicholls <petern3000@gmail.com>
2017-10-27 22:09:52 +1300

    Made modules hot-swappable
    - The NLU now knows what can be done
    - Also fixed a bug such that the json decoder crashed if two packets arrived at the same time.

--- base_classes/action_base.py
+++ base_classes/action_base.py
@@ -4,17 +4,20 @@ class ActionBase(dict):
     ''' The base class for actions/intents '''
     def __init__(self, *args):
         super().__init__(*args)
+        self.assert_items()
 
+    def assert_items(self):
+        ''' Ensure the class has the correct keys '''
         assert(isinstance(self['function'], str))
         if isinstance(self['arguments'], list):
             self['arguments'] = dict.fromkeys(self['arguments'])
         assert(isinstance(self['arguments'], dict))
 
-    # def __str__(self):
-    #     string_representation = "{}(".format(self['function'])
-    #     for arg in self['arguments'].keys():
-    #         string_representation += "{}, ".format(arg)
-    #     if len(self['arguments']) != 0:
-    #         string_representation = string_representation[:-2]
-    #     string_representation += ")"
-    #     return string_representation
+    def __str__(self):
+        string_representation = "{}(".format(self['function'])
+        for arg in self['arguments'].keys():
+            string_representation += "{}, ".format(arg)
+        if len(self['arguments']) != 0:
+            string_representation = string_representation[:-2]
+        string_representation += ")"
+        return string_representation
--- base_classes/module_base.py
+++ base_classes/module_base.py
@@ -78,66 +78,83 @@ class VAModuleBase(object):
         entities_json = json.load(entities_file)['entities']
         entities_file.close()
 
-        for action in actions_json:
-            function_name = action['function']
-            action['function'] = self.get_function_handle(function_name)
-            self.available_actions[function_name] = Action(action, entities_json)
+        for action_dict in actions_json:
+            function_name = action_dict['function']
+            function_handle = self.get_function_handle(function_name)
+
+            action = Action(action_dict)
+            action.set_function_handle(function_handle)
+            action.find_argument_parameters(entities_json)
+            self.available_actions[function_name] = action
 
     def get_function_handle(self, function_name):
         ''' Returns the function handle give a string of it's name '''
         return getattr(self, function_name)
 
 
-class Action(object):
-    ''' An object that can perform an action '''
-    def __init__(self, action_dict, entities_json):
-        self.name = action_dict['name']
-        self.synonyms = action_dict['synonyms']
-        self.function = action_dict['function']
-        self.argument_dict = self.find_arguments(
-            action_dict['arguments'], entities_json)
+class Action(ActionBase):
+    ''' A class that can perform an action '''
+    def assert_items(self):
+        super().assert_items()
+        assert(isinstance(self['name'], str))
+        if 'synonyms' not in self:
+            self['synonyms'] = []
+        assert(isinstance(self['synonyms'], list))
 
+    def set_function_handle(self, function_handle):
+        ''' Sets the function handle '''
+        self.function_handle = function_handle
         self.assert_entities()
 
-    def find_arguments(self, arguments, entities_json):
-        ''' Create a dictionary of each argument '''
+    def find_argument_parameters(self, entities_json):
+        ''' Create a dictionary of each argument with the parameters '''
         argument_dict = dict()
 
-        for action_arg in arguments:
+        for action_arg in self['arguments']:
             if action_arg == 'number':
                 argument_dict[action_arg] = 'number'
             for entity in entities_json:
                 if entity['name'] == action_arg:
-                    if isinstance(entity['parameters'], dict):
-                        entity_list = entity['parameters'].keys()
-                    else:
-                        entity_list = entity['parameters']
-                    argument_dict[action_arg] = entity_list
+                    # if isinstance(entity['parameters'], dict):
+                    #     entity_list = entity['parameters'].keys()
+                    # else:
+                    #     entity_list = entity['parameters']
+                    # argument_dict[action_arg] = entity_list
+                    argument_dict[action_arg] = entity['parameters']
 
-        return argument_dict
+        self['arguments'] = argument_dict
 
     def assert_entities(self):
         ''' Ensure the action arguments match the json entities '''
-        json_args = sorted(self.argument_dict.keys())
-        action_args = sorted(inspect.getargspec(self.function)[0])
+        json_args = sorted(self['arguments'].keys())
+        action_args = sorted(inspect.getargspec(self.function_handle)[0])
         action_args.remove('self')
 
         if action_args != json_args:
             raise AssertionError("Arguments for {} are {} instead of {}".format(
                 self.name, action_args, json_args))
 
+    def __call__(self, **kwargs):
+        ''' Perform the given function '''
+        self.assert_arguments(**kwargs)
+        return self.function_handle(**kwargs)
+
     def assert_arguments(self, **kwargs):
         ''' Ensure the given parameters match their corresponding entities '''
         # action_args = sorted(self.arguments.keys())
         # TODO: This function
 
-    def __call__(self, **kwargs):
-        ''' Perform the given function '''
-        self.assert_arguments(**kwargs)
-        return self.function(**kwargs)
-
 
 class VAModuleProtocol(ClientProtocolBase):
+    def connection_made(self, transport):
+        ''' Callback when the module connects '''
+        super().connection_made(transport)
+        serializable_actions = []
+        for action in self.protocol_handler.available_actions.values():
+            action_dict = dict(action.items())
+            serializable_actions.append(dict(action))
+        self.write_command('set_actions', [serializable_actions])
+
     async def request(self, client_name, action_dict):
         ''' Runs the given action '''
         available_actions = self.protocol_handler.available_actions
